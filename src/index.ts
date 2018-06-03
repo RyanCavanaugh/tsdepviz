b4d028ac1fda3aab2985a5c07512b51ec85c7f4a
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2018-06-03 00:01:56 -0700

    WIP

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import fs = require('fs');
 import path = require('path');
 import * as ts from 'typescript';
 
+// const inputFileName = 'C:/github/typescript/src/services/tsconfig.json';
 const inputFileName = 'C:/github/typescript/src/compiler/tsconfig.json';
-// const inputFileName = 'C:/github/project-references-outfile/cli/tsconfig.json';
 
 const configFile = ts.readJsonConfigFile(inputFileName, ts.sys.readFile);
 const config = ts.parseJsonConfigFileContent(configFile.jsonObject, ts.sys, path.dirname(inputFileName));
@@ -30,7 +30,7 @@ const languageService = ts.createLanguageService({
 });
 program.emit();
 
-run();
+run2();
 
 function run() {
     const lines: string[] = [];
@@ -45,30 +45,49 @@ function run() {
     console.log(lines.join("\r\n"));
 }
 
+function run2() {
+    const lines: string[] = [];
+    const files = program.getSourceFiles().slice();
+    const getLinksCached = cache(getOutboundLinks);
+    files.sort((a, b) => {
+        const aLinks = getLinksCached(a.fileName, a);
+        const bLinks = getLinksCached(b.fileName, b);
+        return aLinks.size - bLinks.size;
+    });
+    for (const file of files) {
+        const links = getOutboundLinks(file);
+        lines.push(`File ${file.fileName} has ${links.size} dependencies`);
+        links.forEach((value, key) => {
+            lines.push(` * ${key}: ${truncate(value.join(", "), 70)}`);
+        });
+    }
+    console.log(lines.join("\r\n"));
+}
+
+function cache<T, U>(func: (arg: T) => U): (key: string, arg: T) => U {
+    const lookup = Object.create(null);
+    return function(key, arg) {
+        if (key in lookup) return lookup[key];
+        return lookup[key] = func(arg);
+    }
+}
+
+function truncate(s: string, n: number) {
+    if (s.length <= n) return s;
+    return s.substr(0, n - 3) + "...";
+}
+
 function shortName(fileName: string) {
     return path.relative(path.dirname(inputFileName), fileName);
 }
 
-function emitFileGraph(sourceFile: ts.SourceFile) {
-    const name = shortName(sourceFile.fileName);
-    const lines: string[] = [];
+function getOutboundLinks(sourceFile: ts.SourceFile) {
     const seenSymbols = new Map<ts.Symbol, boolean>();
+    // Map from fileName to named symbol dependencies
     const dependencies = new Map<string, string[]>();
 
-    lines.push(`    "${name}" [shape=rect]`);
-    ts.forEachChild(sourceFile, walk);
-
-    dependencies.forEach((deps, fileName) => {
-        let label: string
-        if (deps.length > 5) {
-            label = `(${deps.length})`;
-        } else {
-            label = deps.join(", ");
-        }
-        lines.push(`    "${name}" -> "${shortName(fileName)}" [label="${label}"]`);
-    });
-
-    return lines;
+    walk(sourceFile);
+    return dependencies;
 
     function walk(node: ts.Node) {
         const symbol = checker.getSymbolAtLocation(node);
@@ -100,5 +119,23 @@ function emitFileGraph(sourceFile: ts.SourceFile) {
                 ts.forEachChild(node, walk);        
         }
     }
+
+}
+
+function emitFileGraph(sourceFile: ts.SourceFile) {
+    const name = shortName(sourceFile.fileName);
+    const lines: string[] = [];
+    const dependencies = getOutboundLinks(sourceFile);
+    lines.push(`    "${name}" [shape=rect]`);
+    dependencies.forEach((deps, fileName) => {
+        let label: string
+        if (deps.length > 5) {
+            label = `(${deps.length})`;
+        } else {
+            label = deps.join(", ");
+        }
+        lines.push(`    "${name}" -> "${shortName(fileName)}" [label="${label}"]`);
+    });
+    return lines;
 }
 
